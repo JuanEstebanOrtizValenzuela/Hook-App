ad88d5e3b12212eae352339150d65021d8103b8c
JuanOrtiz <juan_ortizva@fet.edu.co>
2024-06-04 11:28:15 -0500

    se agregar los archivos (FormWithCustomHook, Message, MultipleCustomHooks), se ajusta o se agrega el archivo SimpleForm y se agrega nuevos archivos en la carpeta hooks que son: index.js, useFetch.js y useForm.js

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,15 +1,49 @@
+import { useState } from "react"
+import { Message } from "./Message";
+
 export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'juan@gmail.com'
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+
     return (
         <>
             <h1>Formulario Simple</h1>
             <hr />
 
-            <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Username" 
-                name="username" 
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
             />
+
+            <input
+                type="text"
+                className="form-control mt-2"
+                placeholder="juan@gmail.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+
+            {
+                (username === 'strider2') && <Message/>
+            }
         </>
     )
 }
